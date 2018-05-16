afe9812c9a8910472e9665dff54521afa0decd9a
marvin5300 <37504638+marvin5300@users.noreply.github.com>
2018-05-17 01:29:30 +0200

    continued restructuring
    started with the gui

--- build-myon_detector_gui-Desktop_Qt_5_7_1_MinGW_32bit2-Debug/ui_mainwindow.h
+++ build-myon_detector_gui-Desktop_Qt_5_7_1_MinGW_32bit2-Debug/ui_mainwindow.h
@@ -13,9 +13,12 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QFontComboBox>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QProgressBar>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
@@ -25,25 +28,44 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
+    QWidget *centralWidget;
+    QLabel *ipLabel;
+    QProgressBar *uartBuffer;
+    QFontComboBox *ipBox;
+    QLabel *bufferUsageLabel;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
-    QWidget *centralWidget;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(400, 300);
+        MainWindow->resize(569, 473);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        ipLabel = new QLabel(centralWidget);
+        ipLabel->setObjectName(QStringLiteral("ipLabel"));
+        ipLabel->setGeometry(QRect(20, 60, 71, 21));
+        uartBuffer = new QProgressBar(centralWidget);
+        uartBuffer->setObjectName(QStringLiteral("uartBuffer"));
+        uartBuffer->setGeometry(QRect(20, 30, 541, 23));
+        uartBuffer->setValue(24);
+        ipBox = new QFontComboBox(centralWidget);
+        ipBox->setObjectName(QStringLiteral("ipBox"));
+        ipBox->setGeometry(QRect(120, 60, 228, 24));
+        ipBox->setCurrentText(QStringLiteral("localhost"));
+        bufferUsageLabel = new QLabel(centralWidget);
+        bufferUsageLabel->setObjectName(QStringLiteral("bufferUsageLabel"));
+        bufferUsageLabel->setGeometry(QRect(20, 10, 81, 16));
+        MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 569, 25));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        MainWindow->addToolBar(mainToolBar);
-        centralWidget = new QWidget(MainWindow);
-        centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        MainWindow->setCentralWidget(centralWidget);
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
         statusBar = new QStatusBar(MainWindow);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         MainWindow->setStatusBar(statusBar);
@@ -56,6 +78,8 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        ipLabel->setText(QApplication::translate("MainWindow", "connect to:", Q_NULLPTR));
+        bufferUsageLabel->setText(QApplication::translate("MainWindow", "buffer usage:", Q_NULLPTR));
     } // retranslateUi
 
 };
